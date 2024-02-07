9b91c8d8511ddb119aa4a405e1e2e3faf9c40f72
alexandre-Fotso <fyamgroups@gmail.com>
2024-02-07 10:54:56 +0100

    ajout dans le main de la creation dobjet

--- robot/src/main/java/com/combat/cmr/App.java
+++ robot/src/main/java/com/combat/cmr/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+            Robot rob1 = new Robot("bob1");
+            System.out.println("bonjour " + rob1.getNom());
+            Robot rob2 = new Robot("bob2");
+            System.out.println("bonjour " + rob1.getNom());
+            Arena.fight(rob1, rob2);
+         
     }
 }
